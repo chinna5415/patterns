35769a170d2ff7411a9880c56985200c408580c3
chinna5415 <54763489+chinna5415@users.noreply.github.com>
2020-01-18 06:45:34 -0800

    Add files via upload

---
+++ reduced_pattren.py
@@ -0,0 +1,17 @@
+n = int(input("enter the number : "))
+length = n*2-1
+
+for i in range(length):
+    for j in range(length):
+        
+        if i < j : minimum = i
+        else : minimum = j
+
+        if minimum < length - i : minimum = minimum
+        else : minimum = length-i-1
+
+        if minimum < length-j-1 : minimum = minimum
+        else : minimum = length-j-1
+
+        print(n-minimum,end=" ")
+    print()   
