5285d6f5be39cb4fc39b2da001d95bfb5da89e9b
chinna5415 <54763489+chinna5415@users.noreply.github.com>
2020-01-18 20:35:15 -0800

    Add files via upload

---
+++ pyramid_pattern1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int width;
+  printf("enter the width : ");
+  scanf("%d",&width);
+
+  for (int i = 0 ; i < width ; i++) {
+    for (int j = 0 ; j < width ; j++)
+        if (i > j)
+           printf(" ");
+        else
+            printf("*");   
+    printf("\n");
+  }
+
+  return 0;
+}
