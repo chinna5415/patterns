ab97f79aca49d20e2a20a4d64ad2e88ccd2f2466
chinna5415 <54763489+chinna5415@users.noreply.github.com>
2020-01-18 22:32:01 -0800

    Add files via upload

---
+++ Diamond_pattren.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+     ***********
+      *********
+       *******
+        *****
+         ***
+          *
+*/
+
+int main() {
+  int width;
+  printf("enter the width : ");
+  scanf("%d",&width);
+
+  for (int i = 0 ; i < width-1 ; i++) {
+    for (int j = 0 ; j < width+i ; j++) {
+      if (width-i-1 > j)
+            printf(" ");
+        else
+            printf("*");
+    }
+    printf("\n");
+  }
+
+  for (int i = 0 ; i < width ; i++) {
+    for (int j = 0; j < width ; j++) {
+      if (i > j)
+          printf(" ");
+      else
+          printf("*");
+      if (i < j)
+         printf("*");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
