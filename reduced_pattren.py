1ac061b34659008f68370d1d93a0f08c228ff14f
chinna5415 <54763489+chinna5415@users.noreply.github.com>
2020-01-19 12:06:13 +0530

    Update reduced_pattren.py

--- reduced_pattren.py
+++ reduced_pattren.py
@@ -1,6 +1,12 @@
 n = int(input("enter the number : "))
 length = n*2-1
 
+#  3 3 3 3 3
+#  3 2 2 2 3
+#  3 2 1 2 3
+#  3 2 2 2 3
+#  3 3 3 3 3
+
 for i in range(length):
     for j in range(length):
         
