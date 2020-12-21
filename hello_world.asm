ccb7131dbd87a2073cdcf0e78c02f87c973af41b
chinna5415 <54763489+chinna5415@users.noreply.github.com>
2020-12-21 11:43:02 -0500

    Add files via upload

---
+++ hello_world.asm
@@ -0,0 +1,27 @@
+; hello_word.asm
+;
+; Author: Himamanikanta
+; Date: 
+
+global _start
+
+
+section .text:
+
+_start:
+	mov eax, 0x4 			; use the write syscall
+	mov ebx, 1				; use stdout as fd
+	mov ecx, message		; use the message as the buffer
+	mov edx, message_length	; and supply the length
+	int 0x80
+
+	; now gracefully exit
+
+	mov eax, 0x1
+	mov ebx, 0
+	int 0x80
+
+
+section .data:
+	message: db "Hello World!", 0xA
+	message_length equ $-message
